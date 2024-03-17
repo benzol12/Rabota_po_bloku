88340b6c206036f081b1af0d9b0502bd5ebef31b
Bezgodov Oleg <ol.bezgodoff@yandex.ru>
2024-03-17 12:36:33 +0500

    добавлены данные из задачи и два метода

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] words(string[] array) 
+{
+
+}
+void Print(string[] array)
+{
+
+}
+string[] NewArray = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
